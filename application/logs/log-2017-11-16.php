6ef059df7971491afa25fe5c64af4bd4eaceccbd
jseijas25 <luiseijas22@gmail.com>
2017-11-15 23:48:00 -0400

    archivo .gitignore creado.

---
+++ application/logs/log-2017-11-16.php
@@ -0,0 +1,24 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2017-11-16 04:37:21 --> Config Class Initialized
+INFO - 2017-11-16 04:37:21 --> Hooks Class Initialized
+DEBUG - 2017-11-16 04:37:22 --> UTF-8 Support Enabled
+INFO - 2017-11-16 04:37:22 --> Utf8 Class Initialized
+INFO - 2017-11-16 04:37:22 --> URI Class Initialized
+DEBUG - 2017-11-16 04:37:22 --> No URI present. Default controller set.
+INFO - 2017-11-16 04:37:22 --> Router Class Initialized
+INFO - 2017-11-16 04:37:22 --> Output Class Initialized
+INFO - 2017-11-16 04:37:22 --> Security Class Initialized
+DEBUG - 2017-11-16 04:37:22 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-11-16 04:37:22 --> Input Class Initialized
+INFO - 2017-11-16 04:37:22 --> Language Class Initialized
+INFO - 2017-11-16 04:37:22 --> Loader Class Initialized
+INFO - 2017-11-16 04:37:22 --> Helper loaded: url_helper
+INFO - 2017-11-16 04:37:22 --> Helper loaded: form_helper
+INFO - 2017-11-16 04:37:22 --> Helper loaded: html_helper
+INFO - 2017-11-16 04:37:22 --> Helper loaded: datatables_helper
+INFO - 2017-11-16 04:37:23 --> Database Driver Class Initialized
+ERROR - 2017-11-16 04:37:23 --> Severity: Warning --> mysql_connect(): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\CatalogoServicios\system\database\drivers\mysql\mysql_driver.php 136
+ERROR - 2017-11-16 04:37:23 --> Severity: Warning --> mysql_select_db() expects parameter 2 to be resource, boolean given C:\xampp\htdocs\CatalogoServicios\system\database\drivers\mysql\mysql_driver.php 191
+ERROR - 2017-11-16 04:37:23 --> Unable to select database: catalogo_servicios_pic
+INFO - 2017-11-16 04:37:23 --> Language file loaded: language/english/db_lang.php
