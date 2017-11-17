a2ca85b10df3c3e3da334187b2e8b4a5c5aa4d67
jseijas25 <luiseijas22@gmail.com>
2017-11-16 22:35:47 -0400

    header y footer agregado, no funcional

--- application/libraries/fpdf_master.php
+++ application/libraries/fpdf_master.php
@@ -15,6 +15,29 @@ class Fpdf_master {
 		$CI->fpdf = $pdf;
 		
 	}
+function Header()
+   {
 
+       //$this->Image('logo.png',10,8,33);
+
+      $this->SetFont('Arial','B',10);
+
+      $this->Cell(0,10,utf8_decode('Especificación de servicios de integración CI Proyecto                   Fecha'),1,0,'C');
+        $this->Ln(25);
+    
+
+   }
+
+function Footer()
+{
+    // Go to 1.5 cm from bottom
+    $this->SetY(-25);
+    // Select Arial italic 8
+    $this->SetFont('Arial','I',8);
+    //$image=$this->Image('logo.png',180,274,-1400);
+    $this->Cell(0,10,utf8_decode('Gerencia de Tecnología y Soluciones                 pagina '.$this->PageNo().'/{nb}'),1,1,'C');
+    // Print current and total page numbers
+    
+}
 	
 }
